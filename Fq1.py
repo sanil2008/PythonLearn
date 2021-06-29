169445883e8cf939cdb28afe1cbe79afb84064fb
sanil2008 <81163375+sanil2008@users.noreply.github.com>
2021-06-29 21:35:39 +0530

    Add files via upload

---
+++ Fq1.py
@@ -0,0 +1,13 @@
+first = int(input())
+second = int(input())
+if(first > second):
+    print(first)
+if(second > first):
+    print(second)
+
+
+    
+#if(first > second):
+#    print(first)
+#else:
+#    print(second)
