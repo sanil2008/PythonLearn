27b6a1a228416f36545c443dd3f6288a3c9eee53
sanil2008 <81163375+sanil2008@users.noreply.github.com>
2021-06-30 11:09:19 +0530

    Delete Sq1.py

--- Sq1.py
+++
@@ -1,7 +0,0 @@
-number = int(input())
-if(number > 0):
-    print('This number is Positive')
-elif(number < 0):
-    print('This number is Negative')
-else:
-    print('This number is Neither Negative nor Positive')
